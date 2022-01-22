b16834273c9639f25c102d81b29cb9603fbe6ca3
rafael36832 <rafael.brum@ufv.br>
2022-01-21 21:25:28 -0300

    bugs

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -16,7 +16,7 @@ function nav1(){
  // Rodapé Simples
  function foot2(){ 
  	
- 	var pe = "<footer class='bg-secondary pt-4'> <div class='w-100 bg-primary py-3'> <div class='container'> <div class='row pt-2'>       <div class='col-lg-6 col-sm-12'> <p class='text-lg-start text-center text-light light-300'> © Copyright 2021 Universidade Federal de Viçosa All Rights Reserved. </p> </div> <div class='col-lg-6 col-sm-12'> <p class='text-lg-end text-center text-light light-300'> Designed by <a rel='sponsored' class='text-decoration-none text-light' href='https://templatemo.com/' target='_blank'><strong>TemplateMo</strong></a> </p>   </div>  </div> </div> </div> </footer> ";
+ 	var pe = "<footer class='bg-secondary pt-4'> <div class='w-100 bg-primary py-3'> <div class='container'> <div class='row pt-2'>       <div class='col-lg-6 col-sm-12'> <p class='text-lg-start text-center text-light light-300'> © Copyright 2021 Universidade Federal de Viçosa All Rights Reserved. </p> </div> <div class='col-lg-6 col-sm-12'> <p class='text-lg-end text-center text-light light-300'> Designed using <a rel='sponsored' class='text-decoration-none text-light' href='https://templatemo.com/' target='_blank'><strong>TemplateMo</strong></a> </p>   </div>  </div> </div> </div> </footer> ";
  	
  	return pe;
  
